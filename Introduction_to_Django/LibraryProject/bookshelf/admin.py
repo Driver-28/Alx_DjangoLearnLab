f8386d1f5a11b7f58de8dad06d4335dde61f3d2c
Zelalem <zzele57@gmail.com>
2025-02-16 12:54:20 +0300

    customize the admin

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from .models import Book
 # Register your models here.
 class BookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "publication_year")
