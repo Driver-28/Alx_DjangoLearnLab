da4afaf3594dd3aebeea5adacbda40b48fcc8834
Zelalem <zzele57@gmail.com>
2025-03-02 20:47:34 +0300

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -4,4 +4,11 @@ from django import forms
 
 class BookSearchForm(forms.Form):
     search = forms.CharField(max_length=100, required=False, label="Search for Books")
+# LibraryProject/bookshelf/forms.py
 
+from django import forms
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100, label='Your Name')
+    email = forms.EmailField(label='Your Email')
+    message = forms.CharField(widget=forms.Textarea, label='Your Message')
