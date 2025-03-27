1356a21af240aeb252cac310804e25e5eec4ec8b
Zelalem <zzele57@gmail.com>
2025-03-27 06:04:14 +0300

    Implementing the Blog's User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
 
 urlpatterns = [
     # Login, Logout, and Registration views
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),  # For profile view
 ]
